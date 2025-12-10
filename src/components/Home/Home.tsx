ecf8a462fd3391c33afb3ab3ce214d563a4f58e1
jatdalf <70412644+jatdalf@users.noreply.github.com>
2025-12-10 08:48:34 -0300

    app icon set

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -146,7 +146,7 @@ const Home: React.FC = () => {
       </section>
 
       <div className={styles.footer}>
-        <p className={styles.footerText}>© 2025 OCASA. Todos los derechos reservados. (Version: 1.11)</p>  
+        <p className={styles.footerText}>© 2025 OCASA. Todos los derechos reservados. (Version: 1.12)</p>  
         <p className={styles.footerText}>Desarrollado por Jorge Toso</p>
         <Link to="/visitas" className={styles.footerLink}>
         <p className={styles.footerText}>Visitas globales: <strong>{visits !== null ? visits : "Cargando..."}</strong></p>
